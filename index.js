5310956718944b32fb4a954eca08364f660c9c1c
najira <najira_daniela@hotmail.com>
2024-04-28 20:33:48 -0400

    read y archivos estaticos

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-import express from "express";
-const app = express();
+import express from 'express'
+import { readFile } from "fs/promises"
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathFile = __dirname + "/data/deporte.json";
 
-app.get("/", async (req, res) => {
-    res.send("titulo pagina principal")
-})
+// archivos estaticos:
+app.use(express.static(__dirname + '/public'));
 
 
 
-const PORT = process.env.PORT || 5000
+
+// read
+app.get("/deportes", async (req, res) => {
+    try {
+        const stringDeportes = await readFile(pathFile, 'utf8');
+        console.log(stringDeportes)
+        const deportesArray = JSON.parse(stringDeportes);
+        console.log(deportesArray)
+
+        return res.json(deportesArray);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ ok: true });
+    }
+});
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log("servidor escuchando")
-})
+    console.log("Servidor escuchando en el puerto", PORT)
+});
+
